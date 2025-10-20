b6ad583ee304a6911e9010be1fdaf905069e8532
deveps <devepsdev@gmail.com>
2025-10-20 19:41:59 +0200

    build: creació de la base del projecte abasta-API-REST
    Aquests arxius s'han creat automàticament configurant un nou projecte Spring Boot amb les dependències i configuració necessàries en el Spring initialitzr.

---
+++ abasta-api/src/main/java/cat/abasta_back_end/AbastaBackEndApplication.java
@@ -0,0 +1,13 @@
+package cat.abasta_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbastaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbastaBackEndApplication.class, args);
+	}
+
+}
