9b3ccf4d69c6a306f83a92eff1e9447d1e597fd5
unknown <morros_2468@hotmail.com>
2025-10-24 11:07:35 +0200

    fix:Footer

--- abasta-app/src/pages/Home/HomePage.tsx
+++ abasta-app/src/pages/Home/HomePage.tsx
@@ -1,5 +1,16 @@
+import Footer from "../../components/common/Footer";
+
 const HomePage = () => {
-  return <div>Welcome to the Home Page!</div>;
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      {/* Contingut principal */}
+      <main className="flex-fill text-center mt-5">
+        <h1>Welcome to the Home Page!</h1>
+      </main>
+      <Footer />
+    </div>
+  );
 };
+
 
 export default HomePage;
