5d1b74f247f3807df6ddc51456507bbaa1d4cd6d
Carolina Lopez Casanova <carolinalopezcasanova@gmail.com>
2025-10-26 14:18:09 +0100

    refactor: enhance Button component props for improved flexibility and readability

--- abasta-app/src/components/common/Button/Button.tsx
+++ abasta-app/src/components/common/Button/Button.tsx
@@ -1,13 +1,15 @@
-import "./Button.css"
-interface ButtonProps {title:string, onClick:()=> void}
+import './Button.css';
+interface ButtonProps {
+  title: string;
+  type?: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
+}
 
-const Button = ({
-    title, onClick
-}: ButtonProps) =>{
-    return(
-        <button className="btn cta-button" onClick={onClick}>
-            {title}
-        </button>
-    );
+const Button = ({ title, type = 'button', onClick }: ButtonProps) => {
+  return (
+    <button className="btn cta-button" onClick={onClick} type={type}>
+      {title}
+    </button>
+  );
 };
-export default Button
+export default Button;
