7444eee9ac5adc94634cff71dbaf19ede5282e99
Carolina Lopez Casanova <carolinalopezcasanova@gmail.com>
2025-10-26 17:35:59 +0100

    feat: add BenefitsSection with BenefitCard components and styles for enhanced homepage features

--- abasta-app/src/pages/Home/HomePage.tsx
+++ abasta-app/src/pages/Home/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeroSection from './sections/HeroSection/HeroSection';
 import Carousel from '../../components/common/Carousel';
 import FinalCTA from './sections/FinalCTA/FinalCTA';
+import BenefitsSection from './sections/BenefitsSection/BenefitsSection';
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <BenefitsSection />
       <Carousel />
       <FinalCTA />
     </div>
