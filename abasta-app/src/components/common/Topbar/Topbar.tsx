a06ea2256ccae9eb4002ded1a0e8fafccbc07763
unknown <morros_2468@hotmail.com>
2025-10-29 11:08:25 +0100

    Fix:accesibility icon back

--- abasta-app/src/components/common/Topbar/Topbar.tsx
+++ abasta-app/src/components/common/Topbar/Topbar.tsx
@@ -4,8 +4,8 @@ import './Topbar.scss';
 const Topbar = () => {
   return (
     <div className="topbar">
-      <Link to="/" className="back-button d-inline-flex align-items-center">
-        <i className="bi bi-arrow-left-short"></i>
+      <Link to="/" className="back-button d-inline-flex align-items-center" aria-label="Tornar">
+        <i className="bi bi-arrow-left-short"></i> 
       </Link>
     </div>
   );
